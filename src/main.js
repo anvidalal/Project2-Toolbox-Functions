0301272aa7b8176261fc92e76fbe02a5788603ec
Anvi Dalal <anvi@seas.upenn.edu>
2017-01-31 14:19:38 -0500

    added refs

--- src/main.js
+++ src/main.js
@@ -12,13 +12,12 @@ var user_input =
     feather_count : 100,
     feather_size : 1,
     feather_color : 0xaaaaaa, 
-    feather_orientation : 0,
-    flapping_speed : 0.5,
-    flapping_motion : 0,
+    feather_orientation : -1,
+    flapping_speed : 1,
+    flapping_motion : 1,
     wind_speed : 1
 };
 var time = 0;
-var phong = new THREE.MeshPhongMaterial({ color: user_input.feather_color, shininess : 4 });
 var featherGeo;
 var feather_meshes = [];
 var wing = new THREE.Object3D();
@@ -26,36 +25,38 @@ wing.name = "wing";
 
 function alignFeathers(scale, feather_distribution, feather_count, z_offset, color)
 {
-    phong.color.set(color);
-
     var length = feather_distribution * feather_count;
     for (var x = feather_distribution; x < length; x += feather_distribution)
     {
-        var mesh = new THREE.Mesh(featherGeo, phong);
+        var material = new THREE.MeshPhongMaterial({ color, shininess : 0.5 });
+        var mesh = new THREE.Mesh(featherGeo, material);
         var y = impulse(x, user_input.feather_curvature);
         var z = ease_in_out_quadratic(x / length) - z_offset / 4;
         mesh.position.set(x, y, z);
         mesh.scale.set(scale, scale, scale);
         var rot = Math.abs(0.5 * Math.sin(2 * x) * Math.cos(2 * x));
         mesh.rotation.set(Math.PI / 2, 
-            Math.PI / 2 * rot + user_input.feather_orientation, 
+            Math.PI / 2 * rot + user_input.feather_orientation + 1, 
             Math.PI);
         mesh.name = "feather";
+        mesh.receiveShadow = true;
         wing.add(mesh);
     }
 }
 
 function makeWing()
 {
-    for (var i = 1; i <= 6 * user_input.feather_distribution / 0.1; i++)
+    for (var i = 1; i <= 6 * (user_input.feather_distribution + 0.1)/ 0.1; i++)
     {   
         var color = new THREE.Color(user_input.feather_color);
-        color.addScalar(i * 0.2);
+        color.lerp(new THREE.Color(1, 1, 1), 0.1 * i);
         var scale = i * 0.5 * user_input.feather_size;
         var feather_distribution = 0.1 * i;
         var feather_count = user_input.feather_count / i;
         alignFeathers(scale, feather_distribution, feather_count, (i - 1) / 4, color);
     }
+
+    wing.rotation.x =  - Math.PI / 4;
 }
 
 function removeFeathers()
@@ -92,20 +93,14 @@ function onLoad(framework)
     // load a simple obj mesh
     var objLoader = new THREE.OBJLoader();
     objLoader.load('/geo/feather.obj', function(obj) {
-
-        // LOOK: This function runs after the obj has finished loading
         featherGeo = obj.children[0].geometry;
         makeWing();
-        // for (var i = 0; i < feather_meshes.length; i++)
-        // {
-        //     scene.add(feather_meshes[i]);
-        // }
     });
-
+    
     scene.add(wing);
 
     // set camera position
-    camera.position.set(-2, 0, 10);
+    camera.position.set(-2, 10, 15);
     camera.lookAt(new THREE.Vector3(0,0,0));
 
     // scene.add(lambertCube);
@@ -128,32 +123,42 @@ function onLoad(framework)
         removeFeathers();
         makeWing();
     });
-    gui.add(user_input, 'feather_orientation', 0, 2).onChange(function(newVal) {
+    gui.add(user_input, 'feather_orientation', -1, 1).onChange(function(newVal) {
         removeFeathers();
         makeWing();
     });
-    gui.add(user_input, 'wind_speed', 1, 5);
     gui.addColor(user_input, 'feather_color', 0, 2).onChange(function(newVal) {
         removeFeathers();
         makeWing();
     });
+    gui.add(user_input, 'wind_speed', 1, 10);
+    gui.add(user_input, 'flapping_speed', 1, 2);
+    gui.add(user_input, 'flapping_motion', 1, 2);
 }
 
 function flap(wing)
 {
-    var sin = Math.sin(time * user_input.wind_speed / 500);
-    wing.rotation.z += 0.005 * sin;
+    var sin = Math.cos(time * user_input.flapping_motion / 500);
+    sin *= user_input.flapping_speed;
+    wing.rotation.x += 0.02 * sin;
 }
 
 
-function wind(scene, wing)
+function wind(wing)
 {
-
     for (var i = 0; i < wing.children.length; i++)
     {
-        var sin = Math.sin(time * user_input.wind_speed / 500);
-        wing.children[i].rotation.x += 0.005 * sin;
-        wing.children[i].rotation.y -= 0.002 * sin;
+        var rand = Math.random() * 10;
+        var sin = Math.sin(time / 500);
+        sin *= user_input.flapping_speed;
+        if (rand < 5)
+        {
+            wing.children[i].rotation.z += 0.0005 * sin;
+        }
+        else
+        {
+            wing.children[i].rotation.y -= 0.0002 * sin;
+        }
     }
 }
 
@@ -165,7 +170,7 @@ function onUpdate(framework)
         var date = new Date();
         time = date.getTime();
         flap(wing);
-        wind(framework.scene, wing);
+        wind(wing);
     }
 }
 
